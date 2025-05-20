92e17a9caf995823e37c2ffa39853230eabd5d9c
Caterina Paun <caterina.paun@gmail.com>
2025-05-20 12:01:06 -0700

    Update: hello world example with functions

--- javascript/01-hello-world.js
+++ javascript/01-hello-world.js
@@ -1,9 +1,21 @@
 console.log('\n========== JavaScript Hello World ==========\n');
 console.log('Hello there 👋');
 
-printDate();
+// Can we call printDate() on line 5?
+// printDate();
 
 function printDate() {
   const today = new Date();
   console.log(`Today's date is ${today}`);
 }
+
+printDate();
+
+// Can we call todaysDate() on line 15?
+// todaysDate();
+
+const todaysDate = function printTodaysDate() {
+  console.log(`Today's date is ${new Date()}`);
+};
+
+todaysDate();
