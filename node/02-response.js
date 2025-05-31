f44cf4949eb90bbfa97cfda75d1e8e75c6ec2aae
Caterina Paun <caterina.paun@gmail.com>
2025-05-30 21:18:21 -0700

    Update: Node examples

--- node/02-response.js
+++ node/02-response.js
@@ -2,8 +2,12 @@ const http = require('http');
 const port = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello World, Node!</h1>');
+  res.writeHead(208, {
+    'Content-Type': 'text/html',
+    'X-header-response': 'hello',
+    'X-another-header-response': 'world',
+  });
+  res.write('<h1>Hello World, Node making updates </h1>');
   res.end();
 });
 
--- node/06-post-data.js
+++ node/06-post-data.js
@@ -2,14 +2,21 @@
 const http = require('http');
 const port = process.env.PORT || 5001;
 
-const postHTML = `<html><head><title>Post Example</title></head><body>
-  <form method='post'>
-  <label for="name">Name: </label>
-  <input type="text" name="name" id="name"><br />
-  <label for="email">Email: </label>
-  <input type="text" name="email" id="email"><br />
-  <input type='submit'>
-  </form></body></html>`;
+const postHTML = `
+  <html>
+    <head>
+      <title>Post Example</title>
+    </head>
+    <body>
+      <form method='post'>
+        <label for="name">Name: </label>
+        <input type="text" name="username" id="name"><br />
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email"><br />
+        <input type='submit'>
+      </form>
+    </body>
+  </html>`;
 
 const server = http.createServer((req, res) => {
   let body = '';
@@ -18,7 +25,6 @@ const server = http.createServer((req, res) => {
     console.log('on data: ' + body);
   });
   req.on('end', () => {
-    console.log('on end: ' + body);
     res.writeHead(200);
     res.end(postHTML);
   });
